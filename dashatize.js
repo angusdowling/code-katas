12df6555ee91f76713135d4c3da2eaefd6dfa43f
angus.dowling <angusdowling@live.com.au>
2018-04-03 22:37:20 +1000

    Prefill

--- dashatize.js
+++ dashatize.js
@@ -1,14 +1,16 @@
 /**
-* Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
-* 
-* Ex:
-* dashatize(274) -> '2-7-4'
-* dashatize(6815) -> '68-1-5'
-*/
+ * Dashatize
+ * 
+ * Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
+ * 
+ * Ex:
+ * dashatize(274) -> '2-7-4'
+ * dashatize(6815) -> '68-1-5'
+ */
 
 /**
-* Initial
-*/
+ * Initial
+ */
 function dashatize(num) {
     return String(num)
         .replace(/([13579])/g, `-$1-`)
@@ -19,8 +21,8 @@ function dashatize(num) {
 
 
 /**
-* Optimised
-*/
+ * Optimised
+ */
 function dashatize(num) {
     return String(num)
         .replace(/([13579])/g, `-$1-`)
